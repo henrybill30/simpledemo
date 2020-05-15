a3abefcf905fbf070142cba0364ddd22d267e4ca
liu <2536733617@qq.com>
2020-05-15 20:02:15 +0800

    1

--- miniprogram/pages/components/person/index.js
+++ miniprogram/pages/components/person/index.js
@@ -4,26 +4,7 @@ Page({
     showimg: false
   },
   headimgHD:function () {
-    let avatarUrl = this.data.uesrImg
-    console.log('原来的头像', avatarUrl);
-
-    avatarUrl = avatarUrl.split('/');        //把头像的路径切成数组
-
-    //把大小数值为 46 || 64 || 96 || 132 的转换为0
-    if (
-      avatarUrl[avatarUrl.length - 1] && (avatarUrl[avatarUrl.length - 1] == 46 ||
-        avatarUrl[avatarUrl.length - 1] == 64 ||
-        avatarUrl[avatarUrl.length - 1] == 96 ||
-        avatarUrl[avatarUrl.length - 1] == 132)
-    ) {
-      avatarUrl[avatarUrl.length - 1] = 0;
-    }
-
-    avatarUrl = avatarUrl.join('/');   //重新拼接为字符串
-
-    console.log('高清的头像', avatarUrl);
     this.setData({
-      uesrImg: avatarUrl,
       showimg: true
     })
   },
@@ -32,7 +13,6 @@ Page({
       showimg: false
     })
   },
-  noop(){ },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -48,6 +28,23 @@ Page({
         var city = userInfo.city
         var country = userInfo.country
 
+        console.log('原来的头像', avatarUrl);
+
+        avatarUrl = avatarUrl.split('/');        //把头像的路径切成数组
+
+        //把大小数值为 46 || 64 || 96 || 132 的转换为0
+        if (
+          avatarUrl[avatarUrl.length - 1] && (avatarUrl[avatarUrl.length - 1] == 46 ||
+            avatarUrl[avatarUrl.length - 1] == 64 ||
+            avatarUrl[avatarUrl.length - 1] == 96 ||
+            avatarUrl[avatarUrl.length - 1] == 132)
+        ) {
+          avatarUrl[avatarUrl.length - 1] = 0;
+        }
+
+        avatarUrl = avatarUrl.join('/');   //重新拼接为字符串
+
+        console.log('高清的头像', avatarUrl);
         that.setData({
           uesrImg: avatarUrl
         })
