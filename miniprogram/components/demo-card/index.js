bb8287e385ef8cbb41885366985cf933d5d94a14
liu <2536733617@qq.com>
2020-05-23 10:31:31 +0800

    组件拆解小程序样式

---
+++ miniprogram/components/demo-card/index.js
@@ -0,0 +1,14 @@
+Component({
+  properties: {
+    imgurl: {
+      type: String,
+      value: ''
+    },
+    title: String
+  },
+  methods: {
+    to(){
+      console.log('跳转到……')
+    }
+  }
+});
