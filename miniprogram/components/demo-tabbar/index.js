bd67e66097ca1e533b7c46c90d705855727a7011
liu <2536733617@qq.com>
2020-05-30 09:58:26 +0800

    自定义导航栏

--- miniprogram/components/demo-tabbar/index.js
+++ miniprogram/components/demo-tabbar/index.js
@@ -1,16 +1,20 @@
 Component({
   data: {
-    active: 1,
-    icon: [{
-      inactive: 'http://pic.yupoo.com/253673/db990bbf/c6b97c13.png',
-      active: 'http://pic.yupoo.com/253673/70a2c46b/03eb75b0.png'
-    }, {
+    active: 0,
+    icon: [
+    //   {
+    //   inactive: 'http://pic.yupoo.com/253673/db990bbf/c6b97c13.png',
+    //   active: 'http://pic.yupoo.com/253673/70a2c46b/03eb75b0.png'
+    // }, 
+      {
         inactive: 'http://pic.yupoo.com/253673/9d264693/dbb2b6e3.png',
         active: 'http://pic.yupoo.com/253673/3ed5e101/13259079.png'
-      }, {
-        inactive: 'http://pic.yupoo.com/253673/2708e419/75a83e9c.png',
-        active: 'http://pic.yupoo.com/253673/62e13938/9fc95b5f.png'
-      }, {
+      }, 
+      // {
+      //   inactive: 'http://pic.yupoo.com/253673/2708e419/75a83e9c.png',
+      //   active: 'http://pic.yupoo.com/253673/62e13938/9fc95b5f.png'
+      // }, 
+      {
         inactive: 'http://pic.yupoo.com/253673/cbd62833/39f91498.png',
         active: 'http://pic.yupoo.com/253673/9608e3fe/4fa0927b.png'
       }]
