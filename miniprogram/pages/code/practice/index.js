6e0330276c6ea96a50c8ba0874d70c5cf0ed901b
HuangBingYao <969038795@qq.com>
2020-06-12 10:32:29 +0800

    弱互动demo

--- miniprogram/pages/code/practice/index.js
+++ miniprogram/pages/code/practice/index.js
@@ -9,6 +9,7 @@ Page({
     name: '',
     type: '',
     num: 0,
+    List: [],
 
     //view_basic_1数据
     view_basic_1_content: '这是增加了点击态效果的view组件',
@@ -30,7 +31,111 @@ Page({
         type: 'String',
         describe: '显示的文本'
       },
-    ]
+    ],
+    view_basic_1_code: [
+      `<view hover-class="view2" hover-start-time=`,
+      `hover-stay-time=`,
+      `>`,
+      `</view>`
+    ],
+
+    // process_basic_0数据
+    progress_basic_0_percent: 20,
+    progress_basic_0_showinfo: false,
+    progress_basic_0_borderradius: 0,
+    progress_basic_0_fontsize: 16,
+    progress_basic_0_strokewidth: 5,
+    progress_basic_0_activeColor: '#09BB07',
+    progress_basic_0_backgroundColor: '#EBEBEB',
+    progress_basic_0_active: false,
+    progress_basic_0_duration: 30,
+    progress_basic_0_list: [
+      {
+        name: 'percent',
+        type: 'number',
+        describe: '百分比0-100'
+      },
+      {
+        name: 'show-info',
+        type: 'boolean',
+        describe: '在进度条右侧显示百分比'
+      },
+      {
+        name: 'border-radius',
+        type: 'number/String',
+        describe: '圆角大小'
+      },
+      {
+        name: 'font-size',
+        type: 'number/String',
+        describe: '右侧百分比字体大小'
+      },
+      {
+        name: 'stroke-width',
+        type: 'number/String',
+        describe: '进度条线宽度'
+      },
+      {
+        name: 'activeColor',
+        type: 'String',
+        describe: '已选择进度条的颜色'
+      },
+      {
+        name: 'backgroundColor',
+        type: 'String',
+        describe: '未选择进度条的颜色'
+      },
+      {
+        name: 'active',
+        type: 'boolean',
+        describe: '是否显示从左至右的动画'
+      },
+      {
+        name: 'duration',
+        type: 'number',
+        describe: '进度增加1%所需毫秒数'
+      },
+    ],
+    progress_basic_0_code: [
+      `<progress`,
+      `percent=`,
+      `show-info=`,
+      `border-radius=`,
+      `font-size=`,
+      `stroke-width=`,
+      `activeColor=`,
+      `backgroudColor=`,
+      `active=`,
+      `duration=`,
+      `/>`,
+    ],
+  },
+
+  view_basic_1_submit(e){
+    console.log(e.detail.value.view_basic_1_0)
+    var value = e.detail.value
+    value.view_basic_1_0 = parseInt(value.view_basic_1_0)
+    value.view_basic_1_1 = parseInt(value.view_basic_1_1)
+    if(!(value.view_basic_1_0>=0&&value.view_basic_1_0<=10000) || !(value.view_basic_1_1>=0&&value.view_basic_1_1<=20000)){
+      wx.showToast({
+        title: '最好设置正常的属性参数哦~',
+        icon: 'none'
+      })
+      return
+    }
+    this.setData({
+      view_basic_1_starttime: value.view_basic_1_0,
+      view_basic_1_staytime: value.view_basic_1_1,
+      view_basic_1_content: value.view_basic_1_2
+    })
+    wx.showToast({
+      title: '运行成功！试试效果吧~',
+      icon: 'success'
+    })
+  },
+
+  progress_basic_0_submit(e){
+    console.log(e.detail.value)
   },
 
   /**
@@ -42,6 +147,16 @@ Page({
       type: options.type,
       num: options.num
     })
+    if(this.data.name=='view'){
+      this.setData({
+        List: this.data.view_basic_1_list
+      })
+    }
+    if(this.data.name=='progress'){
+      this.setData({
+        List: this.data.progress_basic_0_list
+      })
+    }
   },
 
   /**
